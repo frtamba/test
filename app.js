71426df894a43c1dfa13587995d75ae2c998cfb2
frtamba <121829150+frtamba@users.noreply.github.com>
2023-02-27 20:34:42 +0100

    remove port

--- app.js
+++ app.js
@@ -3,9 +3,5 @@ const port = 8080
 
 http.createServer((req, res) => {
   res.writeHead(200, {'Content-Type': 'text/html'})
-  res.send({
-    message: 'Hello World!'
-  })
-}).listen(port, () => {
-  console.log(`Server listenin at PORT=${port}`)
-})
+  res.end('Hello World!')
+}).listen()
